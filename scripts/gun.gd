ad8d833a66525439d38978734bf48df6c2acd7fb
Austyn <austynlobo@gmail.com>
2024-11-30 15:17:01 +0100

    mouse input detected

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -8,6 +8,13 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		print("You have fired the gun!")
+
+	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
