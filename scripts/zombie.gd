6711d20c30b432dac6c7e58b887d3aeada10b5a1
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2025-01-11 14:06:11 +0100

    Add navigation

--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -4,15 +4,24 @@ extends CharacterBody2D
 @export var target: Node2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
-
+@onready var _navigation_agent: NavigationAgent2D = $Navigation/NavigationAgent2D
 
 func _physics_process(delta: float) -> void:
-	var target_vector = target.position - position
+	var direction = Vector2.ZERO
 	
-	velocity = target_vector.normalized() * speed
+	direction = _navigation_agent.get_next_path_position() - global_position
+	direction = direction.normalized()
+	
+	velocity = direction * speed
+	
 	if velocity.length() == 0:
 		_animated_sprite.play("idle")
 	else:
 		_animated_sprite.play("move")
 		_animated_sprite.flip_h = velocity.x < 0
+	
 	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	_navigation_agent.target_position = target.global_position
